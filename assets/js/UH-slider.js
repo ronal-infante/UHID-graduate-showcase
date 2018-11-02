6020b0dd224b862d785e6ff7612fe778b9793d4b
Ron <rinfante@uh.edu>
2018-11-02 00:41:33 -0500

    JS slider updates

--- assets/js/UH-slider.js
+++ assets/js/UH-slider.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   function carousel() {
 
     var intervals = [];
-    function increment(i){
-    	var j = 0;
+    function increment(i,j){
+    	if(typeof j == 'undefined'){j = 0;}
       return function(){
       	var s = $(".entry").eq(i).find(".slide-container .slide"),
         		d = $(".entry").eq(i).find(".nav-dots label");
@@ -25,22 +25,24 @@ $(document).ready(function(){
     }
     function startSlides(i){
       return function(){
+        clear(i)();
         intervals[i] = setInterval(increment(i), 5000);
       }
     }
+    function dots(i,j){
+      return function(){
+        clear(i)();
+        increment(i,j)();
+      }
+    }
+    
     $('.entry').each(function(i) {
-      // $(".nav-dots label").click(function(){
-      //   console.log(this);
-      // });
-
       startSlides(i)();
+      $(this).find(".nav-dots label").each(function(j){
+        $(this).click(dots(i,j));
+      });
     });
     
   }
   
 });
-
-
-
-    
- 
