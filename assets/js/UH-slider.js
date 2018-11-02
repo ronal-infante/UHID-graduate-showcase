d2c584c22f2e5f9db9653a5b37d67e78349df7e7
Ron <rinfante@uh.edu>
2018-11-01 20:29:44 -0500

    Slider working :)

--- assets/js/UH-slider.js
+++ assets/js/UH-slider.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
-    $("img").click(function(){
-        $(this).hide();
+
+  carousel();
+  
+  function carousel() {
+
+    var intervals = [];
+    function increment(i){
+    	var j = 0;
+      return function(){
+      	var s = $(".entry").eq(i).find(".slide-container .slide"),
+        		d = $(".entry").eq(i).find(".nav-dots label");
+        s.each(function(k){$(this).removeClass("on")});
+        d.each(function(k){$(this).removeClass("on")});
+        if (j > s.length-1) {j = 0}  
+        $(s[j]).addClass("on");
+        $(d[j]).addClass("on");
+        j++;
+      }
+    }
+    function clear(i){
+      return function(){
+        clearInterval(intervals[i]);
+      }
+    }
+    function startSlides(i){
+      return function(){
+        intervals[i] = setInterval(increment(i), 5000);
+      }
+    }
+    $('.entry').each(function(i) {
+      // $(".nav-dots label").click(function(){
+      //   console.log(this);
+      // });
+
+      startSlides(i)();
     });
+    
+  }
+  
 });
+
+
+
+    
+ 
